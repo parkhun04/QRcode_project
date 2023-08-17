505c512aac501b00dbf9ddadbef643cfaa6d2e6e
sunghoon park <2114959@naver.com>
2023-08-17 20:32:32 +0900

    Modifying QE Code

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const qrcode = require('qrcode');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/generate', async (req, res) => {
+app.post('/generate', (req, res) => {
   const ssid = req.body.ssid;
   const password = req.body.password;
 
   const wifiData = {
     ssid: ssid,
     password: password,
-    type: 'WPA',
+    type: 'WPA', // 암호화 유형 (WPA, WEP, 또는 없음)
   };
 
   const wifiString = JSON.stringify(wifiData);
 
-  try {
-    const qrCode = await qrcode.toDataURL(wifiString);
-    res.send(`<img src="${qrCode}" alt="Wi-Fi QR Code"/>`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('QR 코드 생성 중 오류가 발생했습니다.');
-  }
+  qrcode.toDataURL(wifiString, (err, url) => {
+    if (err) {
+      res.send('QR 코드 생성 중 오류가 발생했습니다.');
+    } else {
+      res.send(`<img src="${url}" alt="Wi-Fi QR Code"/>`);
+    }
+  });
 });
 
 app.listen(port, () => {
